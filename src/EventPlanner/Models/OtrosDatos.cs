e97f11b0c56a73470beb385c1a7fec09db188ec6
andresf070893 <andres.fas.070893@gmail.com>
2026-03-27 22:43:49 -0500

    cambis en las clases DAO

---
+++ src/EventPlanner/Models/OtrosDatos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventPlanner.Models
+{
+    public class Programa
+    {
+        public string nombrePrograma { get; set; }
+    }
+    public class  Grupo 
+    {
+        public string nombreGrupo { get; set; }
+    }
+    public class Ficha
+    {
+        public string numeroFicha { get; set; }
+    }
+}
